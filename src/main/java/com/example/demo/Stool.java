c9b89b7154e7d3ce9918f1dcef2c26e415c963bd
IIlya-ipti <79226730+IIlya-ipti@users.noreply.github.com>
2022-04-14 18:33:27 +0300

    update stool

--- src/main/java/com/example/demo/Stool.java
+++ src/main/java/com/example/demo/Stool.java
@@ -5,70 +5,16 @@ import javafx.geometry.Point2D;
 public class Stool {
     private final Block body;
     private final Block leftLeg;
+    private final Block RightLeg;
 
-    private Point2D Point;
-    private final Manifold manileft;
-    private final boolean right;
-
-    Stool(final Block body, final  Block leftLeg, boolean right){
-        this.right = right;
+    Stool(final Block body, final  Block leftLeg,final Block RightLeg){
         this.body = body;
         this.leftLeg = leftLeg;
-        this.manileft = new Manifold(leftLeg,body);
+        this.RightLeg = RightLeg;
 
-        Utility_Functions.bindBlocks(body, leftLeg);
+        body.connect(leftLeg,true);
+        body.connect(RightLeg,false);
+        Utility_Functions.bindBlocks(this.leftLeg,this.RightLeg);
     }
-
-
-    void run(Block block, double t){
-           if(right) {
-               if(block == leftLeg) {
-                   updateRightLeg(leftLeg,body);
-               }else if (block == body){
-                   updateRightLeg(body,leftLeg);
-               }
-           }else{
-               if(block == leftLeg) {
-                   updateLeftLef(leftLeg,body);
-               }else if (block == body){
-                   updateLeftLef(body,leftLeg);
-               }
-
-        }
-    }
-
-    void updateLeftLef(Block one, Block two){
-        manileft.solveCollision();
-        manileft.applyImpulse();
-
-        Point2D point2D = one.getXY().subtract(two.getXY());
-        two.getRectangle().setX(two.getRectangle().getX() + point2D.getX());
-        two.getRectangle().setY(two.getRectangle().getY() + point2D.getY());
-    }
-    void updateRightLeg(Block one, Block two){
-        manileft.solveCollision();
-        manileft.applyImpulse();
-        Point2D point2D = one.getPoints().get(1).subtract(two.getPoints().get(1));
-        two.getRectangle().setX(two.getRectangle().getX() + point2D.getX());
-        two.getRectangle().setY(two.getRectangle().getY() + point2D.getY());
-    }
-
-
-
-
-
-
-    public boolean hasBlock(Block block){
-        return (leftLeg == block) ||(block == body) ;
-    }
-
-    public Block getBody(){
-        return body;
-    }
-
-    public Block getLeftLeg() {
-        return leftLeg;
-    }
-
 
 }
