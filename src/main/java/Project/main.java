57999b435f26b7a41d67e760556d25e7bf9f97c2
IIlya-ipti <79226730+IIlya-ipti@users.noreply.github.com>
2022-04-24 23:20:26 +0300

    update

--- src/main/java/Project/main.java
+++ src/main/java/Project/main.java
@@ -14,25 +14,11 @@ public class main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        Group group = new Group();
-//        Scene scene = new Scene(group,900,900);
-//        Unity unity = new Unity(600,600);
-//        unity.unObjects();
-//        SubScene subScene = unity.RUN();
-//        group.getChildren().add(subScene);
-//        stage.setScene(scene);
-//        stage.show();
         FXMLLoader fxmlLoader = new FXMLLoader(PleaseProvideControllerClassName.class.getResource("template.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
-        //Scene scene = new Scene(root, 300, 275);
-
-        //stage.setTitle("FXML Welcome");
-        //stage.setScene(scene);
-        //stage.show();
     }
     public static void main(String[] args) {
         launch();
