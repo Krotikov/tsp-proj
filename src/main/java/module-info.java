9b5e21f03312fed297e39d3940ba65be7f0d3734
Ivan <jobkuzin@yandex.ru>
2022-05-08 12:04:38 +0300

    add git ignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.example.demo {
     requires javafx.graphics;
     requires lombok;
 
-    opens Project.modules.Physics to javafx.fxml;
     exports Project.modules.Physics;
     exports Project;
     opens Project to javafx.fxml;
